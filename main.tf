e84e60d481b9d9f7956d9724dc50f956cfa8bacf
ianoem <CrunchyByte@protonmail.com>
2025-03-20 13:06:15 -0700

    updated instance_type

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "static_site" {
   ami           = "ami-0c30dbe0df8e646a6"  # Replace with the correct AMI ID
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "StaticSiteInstance"
